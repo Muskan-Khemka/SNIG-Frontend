dea4b73c984bc47d589aaff11eb93392e10dc027
Muskan-Khemka <21muskankhemka@gmail.com>
2024-03-13 11:51:32 +0530

    First commit

---
+++ src/pages/home/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '../../components/ommon/header'
+import Footer from '../../components/ommon/footer'
+
+const HomePage = () => {
+  return (
+    <div>
+        <Header />
+        <Footer />
+    </div>
+  )
+}
+
+export default HomePage
