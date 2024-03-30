6487a22aa549bdfabce13fe6c526a89c171c31a1
Sanjana7939 <75517684+Sanjana7939@users.noreply.github.com>
2024-03-30 18:56:17 +0530

    Update Payment.jsx

--- src/components/payment/Payment.jsx
+++ src/components/payment/Payment.jsx
@@ -1,7 +1,9 @@
 import React, { useState, useContext } from "react";
 import axios from "axios";
-import PayPalCheckoutButton from "./PayPalButton"; 
-import Context from "../../context/context.js"
+import PayPalCheckoutButton from "./PayPalButton";
+import CountContext from "../../context";
+import "./Payment.css";
+
 const amount1 = localStorage.getItem("amount1");
 const token = localStorage.getItem("token");
 const subscriptionPlans = {
@@ -12,51 +14,55 @@ const subscriptionPlans = {
 const App = () => {
   // const [selectedAmount, setSelectedAmount] = useState("");
   const [orderId, setOrderId] = useState("");
-  const context = useContext(Context);
-  const {setSubscribed,subscribed} = context;
-  console.log("subscribed")
-  console.log(subscribed)
+  const context = useContext(CountContext);
+  const { setSubscribed } = context;
   const handlePayPalSuccess = async (data) => {
     try {
       // Make a POST request to your backend with the selected amount
-      const response = await axios.post("http://localhost:4000/api/paypal/checkout", {
-        amount: amount1,
-      },{headers:{
-        Authorization:token
-      }});
-      const {orderId} = response.data;
-      console.log(orderId);
-      setOrderId(orderId);
-      try {
-        const {data: sub} = await axios.get("http://localhost:4000/user/subscription", {
+      const response = await axios.post(
+        "http://localhost:4000/api/paypal/checkout",
+        {
+          amount: amount1,
+        },
+        {
           headers: {
-            Authorization: token
-          }
-        });
-        localStorage.setItem("subscription",sub.subscription);
-        localStorage.setItem("subscribedAt",sub.subscribedAt);
-      } catch (error) {
-        console.error("Error fetching subscription:", error);
-      }
-      try {
-        setSubscribed(true);
-      } catch(error)
-      {
-        console.error("Error setSubscribed:", error);
-      }
+            Authorization: token,
+          },
+        }
+      );
+      const { orderId } = response.data;
+      setOrderId(orderId);
+      setSubscribed(true);
+      // axios request to backend
+      const sub = await axios.get("http://localhost:4000/user/subscription", {
+        headers: {
+          Authorization: token,
+        },
+      });
+      console.log(sub.data.subscription);
+      console.log(sub.data.subscribedAt);
+      localStorage.setItem("subscription", sub.data.subscription);
+      localStorage.setItem("subscribedAt", sub.data.subscribedAt);
     } catch (error) {
       console.error("Error processing PayPal checkout:", error);
     }
   };
 
   return (
-    <div>
+    <div className="payment-container">
       <h2>Selected Plan: {subscriptionPlans[amount1]}</h2>
-      <PayPalCheckoutButton
-        amount={amount1}
-        onSuccess={handlePayPalSuccess}
-      />
-      {orderId && <p>Subscription added successfully. You are now a member {subscriptionPlans[amount1]}. PayPal order ID: {orderId}</p>}
+      <div classname="paypal-button-container">
+        <PayPalCheckoutButton
+          amount={amount1}
+          onSuccess={handlePayPalSuccess}
+        />
+      </div>
+      {orderId && (
+        <p>
+          Subscription added successfully. You are now a member{" "}
+          {subscriptionPlans[amount1]}. PayPal order ID: {orderId}
+        </p>
+      )}
     </div>
   );
 };
